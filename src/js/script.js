3d23fc5c7164bfbc81051a16efd547bfcc4fc5ee
henrique1232H <henriqueau111@gmail.com>
2023-11-10 20:51:51 -0300

    modified:   src/css/style.css 	modified:   src/js/script.js

--- src/js/script.js
+++ src/js/script.js
@@ -4,27 +4,38 @@ const createPopPup = () => {
     const popUpIcon = document.createElement("a");
     const popUpButton = document.createElement("button")
     const popUpDiv = document.createElement("div");
+    const popUpButtonExit = document.createElement("button");
 
     popUp.classList.add("popUp")
     popUpIcon.classList.add("popUpIcon")
     popUpButton.style.margin = 0
     popUpDiv.classList.add("no-show")
 
-    popUpIcon.innerHTML = "Pesquisar"
+    popUpIcon.innerHTML = "Pesquisar";
+    popUpButtonExit.innerHTML = "Sair"
 
     header.appendChild(popUp);
     popUp.appendChild(popUpButton);
     popUpButton.appendChild(popUpIcon);
     header.appendChild(popUpDiv)
 
+    popUpDiv.append(popUpButtonExit);
+
+
     popUpButton.addEventListener("click", () => {
 
-        popUpDiv.classList.toggle("show")
-        popUpDiv.innerHTML = "<h4>Teste </h4>"
+        popUpDiv.classList.toggle("show");
+       
+        popUpDiv.innerHTML += "<h4> Pesquise </h4>"
 
+        console.log(popUpButtonExit)
 
         
     })
+    popUpButtonExit.addEventListener("click", () => {
+        window.alert("Teste");
+    })
+    
 
 }
 
